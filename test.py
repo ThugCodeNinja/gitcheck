2d54e9b6bbb13ef2b444042c3d4e8c2bca362367
risaldarrutam <risaldarrutam@gmail.com>
2024-07-27 12:11:24 +0530

    test changed

--- test.py
+++ test.py
@@ -20,9 +20,7 @@ vectorstore = FAISS.from_documents(split_docss,embeddings)
 os.environ['GROQ_API_KEY']='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'
 retriever=vectorstore.as_retriever()
 retriever.search_kwargs['k'] = 3
-for i in response:
-  print(i.page_content)
-     
+
 st.set_page_config(page_title="Lloyds Mobile Rewards Section", layout="wide")
 
 st.title("Mock Rewards Section")
@@ -31,26 +29,3 @@ st.write("Find exclusive offers and discounts just for you!")
 
 st.sidebar.title("Check your eligibility to turn on the discover mode!")
 # category = st.sidebar.selectbox("Select Category", ["All", "Electronics", "Food", "Groceries"])
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-
-enable_controls = st.session_state.count == 5
-
-def increment_count():
-    st.session_state.count += 1
-
-st.sidebar.title(f'Number of transactions : {st.session_state.count}')
-# st.write(f"Button has been pressed {st.session_state.count} times")
-button=st.sidebar.button("Mock Transaction")
-if button:
-    increment_count()
-
-# Display the updated count
-if enable_controls:
-    st.sidebar.write("You are now eligible for discover mode ")
-    user_input = st.sidebar.text_input("Enter your request or choice of category", key='user_input')
-    print(user_input)
-    response=retriever.get_relevant_documents(query=st.session_state.user_input)
-
-if not enable_controls:
-    st.sidebar.write(f"Keep going {20-st.session_state.count} times.")
